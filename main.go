966fd26b551c9d88bc0e3054010b5d36121354db
nellfs <nellfs@localhost.localdomain>
2023-05-28 16:55:23 -0300

    changes

--- main.go
+++ main.go
@@ -21,35 +21,75 @@ func handleOverlay(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	baseImg, _ := parseImage("assets/nellfs/nellfs.png")
+	baseImgPath := "assets/nellfs/nellfs.png"
+	baseImg, err := parseImage(baseImgPath)
+	if err != nil {
+		http.Error(w, "Failed to load base image", http.StatusInternalServerError)
+		return
+	}
 
-	decoder := json.NewDecoder(r.Body)
-	var request OverlayOptions
-	err := decoder.Decode(&request)
+	var overlayOptions OverlayOptions
+	err = json.NewDecoder(r.Body).Decode(&overlayOptions)
 	if err != nil {
 		http.Error(w, "Invalid request payload", http.StatusBadRequest)
 		return
 	}
 
-	img, err := parseImage("assets/items/anime_eyes.png")
-	if err != nil {
-		log.Fatalf("Unable to open image  %v", err)
+	overlayOptionsPathMap := map[string]string{
+		"red_hat":     "assets/items/red_hat.png",
+		"glasses":     "assets/items/glasses.png",
+		"anime_eyes":  "assets/items/anime_eyes.png",
+		"tie":         "assets/items/tie.png",
+		"wizard_hat":  "assets/items/wizard_hat.png",
+		"bow_tie":     "assets/items/bow_tie.png",
+		"halo":        "assets/items/halo.png",
+		"mustache":    "assets/items/mustache.png",
+		"red_glasses": "assets/items/red_glasses.png",
 	}
 
-	baseImg = createImage(baseImg, img, request.AnimeEyes)
-	baseImg = createImage(baseImg, img, request.Tie)
+	for key, path := range overlayOptionsPathMap {
+		data := getOverlayOption(overlayOptions, key)
+		if data != nil {
+			overlayImg, err := parseImage(path)
+			if err != nil {
+				log.Printf("Failed to load overlay image for key '%s': %v", key, err)
+				continue
+			}
+			overlay := imaging.Resize(overlayImg, data.W, data.H, imaging.NearestNeighbor)
+			baseImg = imaging.Overlay(baseImg, overlay, image.Pt(data.X, data.Y), 1.0)
+		}
+	}
 
-	err = imaging.Save(baseImg, "./result.png")
+	resultPath := "result.png"
+	err = imaging.Save(baseImg, resultPath)
 	if err != nil {
 		log.Fatalf("Failed to save image: %v", err)
+		http.Error(w, "Failed to save image", http.StatusInternalServerError)
 	}
 
 	fmt.Println("Image overlay complete. Result saved as result.png.")
 }
 
-func createImage(baseImg image.Image, overlayImg image.Image, data ImageData) image.Image {
-	overlay := imaging.Resize(overlayImg, data.W, data.H, imaging.NearestNeighbor)
-	result := imaging.Overlay(baseImg, overlay, image.Pt(data.X, data.Y), 1.0)
-
-	return result
+func getOverlayOption(options OverlayOptions, key string) *ImageData {
+	switch key {
+	case "red_hat":
+		return options.RedHat
+	case "glasses":
+		return options.Glasses
+	case "anime_eyes":
+		return options.AnimeEyes
+	case "tie":
+		return options.Tie
+	case "wizard_hat":
+		return options.WizardHat
+	case "bow_tie":
+		return options.BowTie
+	case "halo":
+		return options.Halo
+	case "mustache":
+		return options.Mustache
+	case "red_glasses":
+		return options.RedGlasses
+	}
+	return nil
 }
--- models.go
+++ models.go
@@ -8,13 +8,13 @@ type ImageData struct {
 }
 
 type OverlayOptions struct {
-	RedHat     ImageData `json:"red_hat"`
-	Glasses    ImageData `json:"glasses"`
-	AnimeEyes  ImageData `json:"anime_eyes"`
-	Tie        ImageData `json:"tie"`
-	WizardHat  ImageData `json:"wizard_hat"`
-	BowTie     ImageData `json:"bow_tie"`
-	Halo       ImageData `json:"halo"`
-	Mustache   ImageData `json:"mustache"`
-	RedGlasses ImageData `json:"red_glasses"`
+	RedHat     *ImageData `json:"red_hat"`
+	Glasses    *ImageData `json:"glasses"`
+	AnimeEyes  *ImageData `json:"anime_eyes"`
+	Tie        *ImageData `json:"tie"`
+	WizardHat  *ImageData `json:"wizard_hat"`
+	BowTie     *ImageData `json:"bow_tie"`
+	Halo       *ImageData `json:"halo"`
+	Mustache   *ImageData `json:"mustache"`
+	RedGlasses *ImageData `json:"red_glasses"`
 }
